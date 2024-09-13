5018c00f49971aa85ffdbed68884bfd96ab287be
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2024-09-13 16:13:12 +0300

    Start implementing forward problem

---
+++ deepinelasticscattering.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+"""
+inversedipole implementation written in collaboration by H. Hänninen, A. Kykkänen and H. Schlüter
+Copyright 2024
+
+deepinelasticscattering module implements calculation of DIS cross sections in the dipole picture.
+"""
+
+import math
+import numpy
+import scipy.integrate as integrate
+import scipy.special as special
+
+Nc = 3.0
+sumef_light = 6.0/9.0 # light quarks uds only.
+CF = 4.0/3.0
+alphaem = 1.0/137.0
+lambdaqcd = 0.241 #GeV
+structurefunfac = 1./((2*math.pi)**2 * alphaem)
+
+def Sq(z):
+    return z**2
+
+class Dipole:
+    def __init__(self, q0sq, ec, gamma):
+        self.q0sq = q0sq
+        self.ec = ec
+        self.gamma = gamma
+    
+    def scattering_S_x0(self, r):
+        return math.exp(-(r**2*self.q0sq)/4 * math.log(1/(r*lambdaqcd)+self.ec*math.e))
+
+def sigma_reduced(x,Q,y):
+    fac = structurefunfac * Sq(Q)
+    FL = fac * sigma_L
+    FT = fac * sigma_T
+    F2 = FL + FT
+    fy = y**2/(1+(1-y)**2)
+    sigmar = F2 - fy * FL
+    return sigmar
+
+def dipole_scattering_amplitude_S(r,x):
+    S = 0
+    return S
+
+def dipole_amplitude_N(r,x):
+    N = 1 - dipole_scattering_amplitude_S(r,x)
+    return N
+
+def rapidity_X(x, Qsq):
+    X = 0
+    return X
+
+def bessel_K0(r):
+    K0 = special.kn(0, r)
+    return K0
+
+def bessel_K1(r):
+    K1 = special.kn(1, r)
+    return K1
+
+def dis_quarkantiquark_dipole_wavefunction_pol_T(Q,z,r,qmass):
+    Qsq = Q**2
+    rapidity_X = rapidity_X(x,Qsq)
+    
+    af = math.sqrt( Sq(Q)*z*(1.0-z) + Sq(qmass) )
+    impactfac = (1.0-2.0*z+2.0*Sq(z))*Sq(af * bessel_K1(af * r)) + Sq(qmass * bessel_K0(af * r))
+    psi_squared = impactfac * r # r here is from the jacobian from the change to polar coordinates. Old choice to have it here.
+    return psi_squared
+
+def dis_quarkantiquark_dipole_wavefunction_pol_L(Q,z,r,qmass):
+    Qsq = Q**2
+    rapidity_X = rapidity_X(x,Qsq)
+    af = math.sqrt( Qsq*z*(1-z) + qmass**2 )
+    impactfac = 4.0*(Q*(z)*(1.0-z)*bessel_K0(af * r))**2
+    # res = (1.0-(dipole_scattering_amplitude_S(r, rapidity_X))) * impactfac * r  # Old code for reference. Includes the dipole which is now done elsewhere
+    psi_squared = impactfac * r # r here is from the jacobian from the change to polar coordinates. Old choice to have it here.
+    return psi_squared
+
+def sigma_T(x,Q):
+    sigmat = 0
+    return sigmat
+
+def sigma_L(x,Q):
+    sigmal = 0
+    return sigmal
---
+++ inversedipole.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+inversedipole implementation written in collaboration by H. Hänninen, A. Kykkänen and H. Schlüter
+Copyright 2024
+"""
+
+__author__ = "Henri Hänninen, Antti Kykkänen, and Hjørdis Schlüter"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Henri Hänninen", "Antti Kykkänen", "Hjørdis Schlüter"]
+__license__ = "MIT"
+__version__ = "0.0.0"
+__maintainer__ = "Henri Hänninen"
+__email__ = "henri.j.hanninen@jyu.fi"
+__status__ = "Development"
+
+import sys
+
+import inversedipole.deepinelasticscattering
+
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
