6ed14515da59593105498c7d6fd477e715371ae9
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2025-03-05 13:44:32 +0200

    Data binning in s and xbj, add two datasets

--- data_manage.py
+++ data_manage.py
@@ -1,3 +1,4 @@
+from math import sqrt
 import numpy as np
 
 def get_data(file, simulated=True):
@@ -61,3 +62,76 @@ def load_dipole(file):
     ]
     data = np.loadtxt(file, dtype=dtype, skiprows=skip_to)
     return data
+
+def count_bins(arr, min=0):
+    counted = []
+    for v in arr:
+        if v not in counted:
+            cnt = arr.count(v)
+            if cnt>=min:
+                print(v, cnt)
+            counted.append(v)
+    return counted
+
+
+if __name__=="__main__":
+    data_sigmar = get_data("./data/hera_II_combined_sigmar.txt", simulated=False)
+    qsq_vals = data_sigmar["qsq"]
+    y_vals = data_sigmar["y"]
+    s_vals = []
+    # sqrt(s)   N points
+    # 318.1     644
+    # 300.3     112
+    # 251.5     260
+    # 224.9     210
+    s_bin = 318.1
+    binned_data = []
+    for datum in data_sigmar:
+        (qsq, xbj, y, sigmar, sig_err, staterruncor, tot_noproc, relative_err) = datum
+        s = round(sqrt(qsq/(y*xbj)),1)
+        if s == s_bin:
+            binned_data.append(datum)
+        # s_vals.append(round(sqrt(s),1))
+        # print(datum, "sqrt(s)= ", sqrt(s))
+    
+    dtype = [
+        ('qsq', float),
+        ('xbj', float),
+        ('y', float),
+        ('sigmar', float),
+        ('sigmarerr', float),
+        # ('theory', float),
+        ('StatErrUncor', float),
+        ('tot_noproc', float),
+        ('%', float),
+        ]
+    binned_data = np.array(binned_data, dtype=dtype)
+    # x_vals = binned_data["xbj"].tolist()
+    # count_bins(x_vals, 20)
+    # xbj    N
+    # 0.002  21
+    # 0.0032 24
+    # 0.005  24
+    # 0.008  23
+    # 0.013  30
+    # 0.02   35
+    # 0.032  33
+    # 0.05   30
+    # 0.08   30
+    # 0.13   31
+    # 0.18   33
+    # 0.25   31
+    # 0.4    33
+
+    # x_bin = 0.013
+    x_bin = 0.002
+    binned_data2_s_xbj = []
+    for datum in binned_data:
+        (qsq, xbj, y, sigmar, sig_err, staterruncor, tot_noproc, relative_err) = datum
+        if xbj==x_bin:
+            binned_data2_s_xbj.append(datum)
+    
+    # print(binned_data2_s_xbj)
+    for d in binned_data2_s_xbj:
+        (qsq, xbj, y, sigmar, sig_err, staterruncor, tot_noproc, relative_err) = d
+        print(qsq, xbj, y, sigmar, sig_err, staterruncor, tot_noproc, relative_err)
