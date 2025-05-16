f7c78696e3cd546017c73ae025bd2b50cf0efd44
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2025-05-16 12:12:41 +0300

    Update reconstruct_dipole_matlab.m
    Improvements to plotting and automation

--- reconstruct_dipole_matlab.m
+++ reconstruct_dipole_matlab.m
@@ -4,9 +4,14 @@ addpath(genpath('./dependencies'))
 close all
 clear all
 
-xbj_bin = "0.0032";
-use_real_data = true;
-use_charm = true;
+% xbj_bin = "0.0001"
+xbj_bin = "1e-05"
+r_steps = 400
+r_steps_str = strcat("r_steps",int2str(r_steps));
+use_real_data = false
+% use_charm = false
+% use_real_data = true
+use_charm = true
 
 charm_opt = "lightonly";
 if (use_charm)
@@ -22,7 +27,7 @@ data_path = './exports/';
 data_files = dir(fullfile(data_path,'*.mat'));
 for k = 1:numel(data_files)
     fname = data_files(k).name;
-    if (contains(fname, xbj_bin) && contains(fname, data_type) && contains(fname, charm_opt))
+    if (contains(fname, xbj_bin) && contains(fname, data_type) && contains(fname, charm_opt) && contains(fname, r_steps_str))
         run_file = fname;
     end
 end
@@ -30,10 +35,11 @@ load(strcat(data_path, run_file))
 
 % if using real data, need to load reference fit dipole separately
 sim_type = "simulated";
+dip_file = 'exp_fwdop+data_simulated-lo-sigmar_MVgamma_dipole-lightpluscharm_newbins_r_steps200_xbj0.0008.mat'; % backup file to have something
 for k = 1:numel(data_files)
     fname = data_files(k).name;
-    if (contains(fname, xbj_bin) && contains(fname, sim_type) && contains(fname, charm_opt))
-        dip_file = fname;
+    if (contains(fname, xbj_bin) && contains(fname, sim_type) && contains(fname, charm_opt) && contains(fname, r_steps_str))
+        dip_file = fname
     end
 end
 dip_data = load(strcat(data_path, dip_file));
@@ -56,7 +62,7 @@ end
 
 %%
 
-ivec3= 1:200;
+ivec3= 1:r_steps;
 % ivec3 = 1:20:200;
 
 x = discrete_dipole_N;
@@ -91,7 +97,8 @@ N=length(x);
 
 
 %%
-lambda = [1,3e-1,1e-1,3e-2,1e-2,3e-3,1e-3,3e-4,1e-4,3e-5];
+% lambda = [1,3e-1,1e-1,3e-2,1e-2,3e-3,1e-3,3e-4,1e-4,3e-5];
+lambda = [6e-1,5e-1,4e-1,3e-1,1e-1,9e-2,8e-2,7e-2,5e-2,3e-2,1e-2,9e-3,7e-3,5e-3,3e-3,1e-3];
 X_tikh = tikhonov(UU,sm,XX,b,lambda);
 errtik = zeros(size(lambda));
 
@@ -101,21 +108,32 @@ end
 [m,mI]=min(errtik);
 
 figure(1)
-plot(ivec3,x','-',ivec3,X_tikh(:,mI),'--','LineWidth',3)
-leg=legend('true','PTik')
-set(leg,'FontSize',14);
-title('Preconditioned Tikhonov with xbj = 0.002','FontSize',16)
+% plot(ivec3,x','-',ivec3,X_tikh(:,mI),'--','LineWidth',2)
+r_grid(end) = []; 
+loglog(r_grid',x','-',r_grid',X_tikh(:,mI),'--','LineWidth',2)
+leg=legend('true',['PTik lambda=', num2str(lambda(mI))]);
+set(leg,'FontSize',12);
+set(leg,'Location',"northwest");
+title(['Preconditioned Tikhonov with xbj=',num2str(xbj_bin)],'FontSize',12)
 pos1 = get(gcf,'Position'); % get position of Figure(1) 
 set(gcf,'Position', pos1 - [pos1(3)/2,0,0,0]) % Shift position of Figure(1) 
 
 %% Comparing lambdas
 figure(2)
+plot(1:N,x','-','LineWidth',1)
+hold on
 for i = 1:length(lambda)
-plot(1:N,x','-',1:N,X_tikh(:,i),'--','LineWidth',3)
+% plot(1:N,x','-',1:N,X_tikh(:,i),'--','LineWidth',1)
+plot(1:N,X_tikh(:,i),'--','LineWidth',1)
 ylim([-0.4,1.5])
 hold on
 end
-title('Reconstructions at various lambda, xbj = 0.002','FontSize',16)
+title(['Reconstruction with various lambda xbj=',num2str(xbj_bin)],'FontSize',12)
+legend_lambdas = cellstr(num2str(lambda', 'l=%-f'));
+legend_items = cat(1, 'true', legend_lambdas);
+leg=legend(legend_items);
+set(leg,'FontSize',10);
+set(leg,'Location',"southeast");
 hold off
 pos2 = get(gcf,'Position');  % get position of Figure(2) 
 set(gcf,'Position', pos2 + [pos1(3)/2,0,0,0]) % Shift position of Figure(2)
@@ -126,9 +144,13 @@ q2vals = qsq_vals;
 bend = A*X_tikh(:,mI);
 
 figure(3)
-plot(q2vals,b,'bx',q2vals,bfit,'ro',q2vals,bend,'go','LineWidth',1)
-legend('b - measurements','b - fit','b - reconstruction')
-title('How well does the reconstruction fit the data for xbj = 0.002','FontSize',12)
+% plot(q2vals,b,'bx-',q2vals,bfit,'ro',q2vals,bend,'go','LineWidth',1)
+% semilogx(q2vals,b,'bx',q2vals,bfit,'ro',q2vals,bend,'go','LineWidth',0.4)
+loglog(q2vals,b,'bx',q2vals,bfit,'ro',q2vals,bend,'go','LineWidth',0.4)
+leg=legend('b - measurements','b - fit','b - reconstruction')
+title(['How well does the reconstruction fit the data for xbj=',num2str(xbj_bin)],'FontSize',12)
+set(leg,'FontSize',10);
+set(leg,'Location',"southeast");
 pos3 = get(gcf,'Position');  % get position of Figure(3) 
 set(gcf,'Position', pos3 + [3*pos2(3)/2,0,0,0]) % Shift position of Figure(2)
 
@@ -147,3 +169,8 @@ set(gcf,'Position', pos3 + [3*pos2(3)/2,0,0,0]) % Shift position of Figure(2)
 % plot(q2vals,b,'bo-',q2vals,bfit,'ro-',q2vals,b1noise,'go-','LineWidth',2)
 
 
+% EXPORTING RESULTS
+
+%% todo export reconstructed dipole
+
+%% with real data, print max / peak and xbj_bin to a file
