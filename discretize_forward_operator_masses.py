c3ae6ba4bf2083738deceb6d16d9fa08ff0e1760
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2025-08-21 23:10:43 +0300

    Begin implementing discretization with quark mass schemes

--- discretize_forward_operator_masses.py
+++ discretize_forward_operator_masses.py
@@ -12,8 +12,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from data_manage import load_dipole, get_data, read_sigma02
-from deepinelasticscattering import fwd_op_sigma_reduced, fwd_op_sigma_reduced_udsc
-
+from deepinelasticscattering import fwd_op_sigma_reduced, fwd_op_sigma_reduced_udscb
+from quark_mass_schemes import mass_scheme_standard, mass_scheme_pole, mass_scheme_mq_Mpole, mass_scheme_mq_mq
 
 
 def z_inted_fw_sigmar(datum, r_grid, sigma02):
@@ -95,24 +95,18 @@ def z_inted_fw_sigmar_udsc(datum, r_grid, sigma02):
         fwd_op_trapez[i][i+1] = z_int_ii/2.*delta_r
     return fwd_op_trapez
 
-def z_inted_fw_sigmar_udsc_riem_uniftrapez(datum, r_grid, sigma02):
+def z_inted_fw_sigmar_udscb_riem_uniftrapez(datum, r_grid, sigma02, quark_masses):
     if len(datum)==6:
         (xbj, qsq, y, sigmar, fl, ft) = datum
     else:
         (qsq, xbj, y, sigmar, sig_err, staterruncor, tot_noproc, relative_err) = datum
     r_grid=r_grid[0]
     z_inted_points = []
-    # Riemann sum, lower boundary evaluation
-    # for i, r in enumerate(r_grid[:-1]):
-    #     delta_r = r_grid[i+1]-r_grid[i]
-    #     z_inted_fw_sigmar_val = integrate.quad(lambda z: sigma02*fwd_op_sigma_reduced_udsc(qsq, y, z, r), 0, 1, epsrel=1e-4)
-    #     z_inted_points.append((r, z_inted_fw_sigmar_val[0]*delta_r))
-    # return np.array(z_inted_points)
 
     # Trapezoid rule, uniform interval width
     for i, r in enumerate(r_grid[:-1]):
         delta_r = r_grid[i+1]-r_grid[i]
-        z_inted_fw_sigmar_val = integrate.quad(lambda z: sigma02*fwd_op_sigma_reduced_udsc(qsq, y, z, r), 0, 1, epsrel=1e-4)
+        z_inted_fw_sigmar_val = integrate.quad(lambda z: sigma02*fwd_op_sigma_reduced_udscb(qsq, y, z, r, quark_masses), 0, 1, epsrel=1e-4)
         if ((i==0) or (i==len(r_grid[:-1])-1)):
             z_inted_points.append((r, z_inted_fw_sigmar_val[0]*delta_r/2))
         else:
@@ -186,13 +180,25 @@ def export_discrete(dipfile, xbj_bin, data_sigmar, parent_data_name, sigma02, in
 
 
 
-def export_discrete_uniform(dipfile, xbj_bin, data_sigmar, parent_data_name, sigma02, include_dipole=True, use_charm=False, use_unity_sigma0=False):
+def export_discrete_uniform(dipfile, mass_scheme, xbj_bin, data_sigmar, parent_data_name, include_dipole=True, use_unity_sigma0=False):
     interpolated_r_grid = []
     rmin=5e-3
     rmax=25 # tightening rmin and rmax help a little with the discretization precision
-    r_steps=500 # 500 by default for simulated!
-    # r_steps=256 # still good for simulated! (maybe, might be a bit too bad at large Q^2)
+    # r_steps=500 # 500 by default for simulated!
+    r_steps=256 # still good for simulated! (maybe, might be a bit too bad at large Q^2)
     # r_steps=128 # this leads to >2%, maybe up to 4-5%, errors at worst. Not good enough.
+
+    if mass_scheme == "standard":
+        quark_masses = mass_scheme_standard
+    elif mass_scheme == "pole":
+        quark_masses = mass_scheme_pole
+    elif mass_scheme == "mqMpole":
+        quark_masses = mass_scheme_mq_Mpole
+    elif mass_scheme == "mqmq":
+        quark_masses = mass_scheme_mq_mq
+    else:
+        print("BAD MASS SCHEME")
+        exit()
 
     r=rmin
     while len(interpolated_r_grid)<r_steps+1:
@@ -221,21 +227,13 @@ def export_discrete_uniform(dipfile, xbj_bin, data_sigmar, parent_data_name, sig
             discrete_N_vals.append(discr_N)
         vec_discrete_N = np.array(discrete_N_vals)
 
-    # return 0 # use this breakpoint for interpolator testing.
-
-    real_sigma = sigma02
+    # real_sigma = sigma02
     if use_unity_sigma0:
-        # if dipfile:
-        #     print("Using unity sigma, multiplying sigma02 into the dipole amplitude")
-        #     vec_discrete_N = vec_discrete_N * sigma02
         sigma02 = 1
 
-    print("Generating discrete forward operator using uniform intervals. use_charm=", use_charm)
     with multiprocessing.Pool(processes=16) as pool:
-        if use_charm:
-            fw_op_vals_z_int = pool.starmap(z_inted_fw_sigmar_udsc_riem_uniftrapez, ((datum, (interpolated_r_grid,), sigma02) for datum in data_sigmar))
-        else:
-            fw_op_vals_z_int = pool.starmap(z_inted_fw_sigmar_riem_uniftrapez, ((datum, (interpolated_r_grid,), sigma02) for datum in data_sigmar))
+        fw_op_vals_z_int = pool.starmap(z_inted_fw_sigmar_udsc_riem_uniftrapez, ((datum, (interpolated_r_grid,), sigma02, quark_masses) for datum in data_sigmar))
+
 
     fw_op_datum_r_matrix = []
     for array in fw_op_vals_z_int:
@@ -243,10 +241,7 @@ def export_discrete_uniform(dipfile, xbj_bin, data_sigmar, parent_data_name, sig
     fw_op_datum_r_matrix = np.array(fw_op_datum_r_matrix)
 
     # Filename settings
-    if use_charm:
-        str_id_charm = "_lightpluscharm"
-    else:
-        str_id_charm = "_lightonly"
+    str_id_qmass = mass_scheme + "_"
     if use_unity_sigma0:
         str_unity_sigma02 = "_unitysigma"
     else:
@@ -260,22 +255,22 @@ def export_discrete_uniform(dipfile, xbj_bin, data_sigmar, parent_data_name, sig
         sigmar_errs = []
 
     # Export
-    exp_folder = "./export_fwd_IUSinterp_fix/"
-    base_name = exp_folder+"exp_fwdop_v3-1+data_"
+    exp_folder = "./export_hera_data/"
+    base_name = exp_folder+"exp_fwdop_qms_hera_"
     if include_dipole:
         # Simulated data and dipole
         dscr_sigmar = np.matmul(fw_op_datum_r_matrix, vec_discrete_N)
-        for d, s in zip(data_sigmar, dscr_sigmar):
-            print(d, d["sigmar"], real_sigma*s, real_sigma*s/d["sigmar"])
+        # for d, s in zip(data_sigmar, dscr_sigmar):
+        #     print(d, d["sigmar"], real_sigma*s, real_sigma*s/d["sigmar"])
         mat_dict = {
             "forward_op_A": fw_op_datum_r_matrix, 
             "discrete_dipole_N": vec_discrete_N, 
             "r_grid": interpolated_r_grid,
             "qsq_vals": qsq_vals,
             "sigmar_vals": sigmar_vals,
-            "real_sigma": real_sigma
+            # "real_sigma": real_sigma
             }
-        savemat(base_name+parent_data_name+str_id_charm+str_unity_sigma02+"_r_steps"+str(r_steps)+"_xbj"+str(xbj_bin)+".mat", mat_dict)
+        savemat(base_name+parent_data_name+str_id_qmass+str_unity_sigma02+"_r_steps"+str(r_steps)+"_xbj"+str(xbj_bin)+".mat", mat_dict)
     else:
         # Real data without dipole // use_real_data !== include_dipole (opposite)
         mat_dict = {
@@ -283,71 +278,65 @@ def export_discrete_uniform(dipfile, xbj_bin, data_sigmar, parent_data_name, sig
             "r_grid": interpolated_r_grid,
             "qsq_vals": qsq_vals,
             "sigmar_vals": sigmar_vals,
+            TODO CORRELATED UNCERTAINTIES ALSO!
             "sigmar_errs": sigmar_errs,
-            "real_sigma": real_sigma
             }
-        savemat(base_name+parent_data_name+str_id_charm+str_unity_sigma02+"_r_steps"+str(r_steps)+".mat", mat_dict)
+        savemat(base_name+parent_data_name+str_id_qmass+str_unity_sigma02+"_r_steps"+str(r_steps)+".mat", mat_dict)
     
     return 0
 
 
 
-def run_export(use_charm, use_real_data, fitname_i=None):
+def run_export(mass_scheme, use_real_data, fitname_i=None):
     """Recostruct the dipole amplitude N from simulated reduced cross section data."""
 
     ###################################
     ### SETTINGS ######################
     ###################################
 
-    # use_charm = False
-    # use_charm = True
-    # use_real_data = False
-    # use_real_data = True
     use_unity_sigma0 = True
 
-    #        0        1        2        3           4
-    fits = ["MV", "MVgamma", "MVe", "bayesMV4", "bayesMV5"]
-    if not fitname_i:
-        fitname = fits[3]
-    else:
-        fitname = fits[fitname_i]
-    # fitname = fits[3]
+    # #        0        1        2        3           4
+    # fits = ["MV", "MVgamma", "MVe", "bayesMV4", "bayesMV5"]
+    # if not fitname_i:
+    #     fitname = fits[3]
+    # else:
+    #     fitname = fits[fitname_i]
 
     ####################
     # Reading data files
     data_path = "./data/paper1/"
-    dipole_files = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and \
-        'dipole_fit_'+fitname+"_" in i]
-    print(dipole_files)
-    xbj_bins = [float(Path(i).stem.split("xbj")[1]) for i in dipole_files]
-    print(xbj_bins)
+    # dipole_files = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and \
+    #     'dipole_fit_'+fitname+"_" in i]
+    # print(dipole_files)
+    # xbj_bins = [float(Path(i).stem.split("xbj")[1]) for i in dipole_files]
+    # print(xbj_bins)
 
-    if use_charm:
-        sigmar_files = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and \
-            'sigmar_'+fitname+"_dipole-lightpluscharm" in i]
-    else:
-        sigmar_files = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and \
-            'sigmar_'+fitname+"_dipole." in i]
-    print(sigmar_files)
-    hera_sigmar_files = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and \
-        "heraII_filtered" in i]
+    # if use_charm:
+    #     sigmar_files = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and \
+    #         'sigmar_'+fitname+"_dipole-lightpluscharm" in i]
+    # else:
+    #     sigmar_files = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and \
+    #         'sigmar_'+fitname+"_dipole." in i]
+    # print(sigmar_files)
     
     # loading data for the corret fit setup
-    sig_file = data_path + sigmar_files[0]
-    data_sigmar = get_data(sig_file)
-    if len(sigmar_files)>1:
-        print("MORE THAN ONE SIGMAR_FILE AT LOAD TIME??")
-        print("Using first file.")
-        print(sigmar_files)
-        exit(1)
-    qsq_vals = data_sigmar["qsq"]
-    y_vals = data_sigmar["y"]
-    sigma02=read_sigma02(sig_file)
-    print("sigma02 read as: ", sigma02, isinstance(sigma02, float))
-    if use_unity_sigma0:
-        # sigma02=1 ## Instead multiply the sigma into the fit dipole to train lambda?
-        print("Using unity sigma02 with real sigma02 = ", sigma02)
+    # sig_file = data_path + sigmar_files[0]
+    # data_sigmar = get_data(sig_file)
+    # if len(sigmar_files)>1:
+    #     print("MORE THAN ONE SIGMAR_FILE AT LOAD TIME??")
+    #     print("Using first file.")
+    #     print(sigmar_files)
+    #     exit(1)
+    # qsq_vals = data_sigmar["qsq"]
+    # y_vals = data_sigmar["y"]
+    # sigma02=read_sigma02(sig_file)
+    # print("sigma02 read as: ", sigma02, isinstance(sigma02, float))
+    # if use_unity_sigma0:
+    #     print("Using unity sigma02 with real sigma02 = ", sigma02)
 
+    hera_sigmar_files = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and "heraII_filtered" in i]
+    
     #############################################
     # Discretizing and exporting forward problems
     if use_real_data:
@@ -355,21 +344,19 @@ def run_export(use_charm, use_real_data, fitname_i=None):
         print(hera_sigmar_files)
         xbj_bin_vals = [float(Path(i).stem.split("xbj")[1]) for i in hera_sigmar_files]
         print(xbj_bin_vals)
-        # exit()
-        # sigma02=35.6952 #bayesMV4 fit value
-        # sigma02=1 # TODO TEST IF WE CAN RECOVER THE OVERALL SIZE CORRECTLY -> independent xbj dependence of sigma02
-        # sigma02=42.0125*0.4 #Manual scaling
         for sig_file in hera_sigmar_files:
             print("Loading data file: ", sig_file)
             data_sigmar = get_data(data_path + sig_file, simulated=False)
             xbj_bin = float(Path(sig_file).stem.split("xbj")[1])
-            print("Discretizing forward problem for real data file: ", sig_file, " at xbj=", xbj_bin)
+            print("Discretizing forward problem for real data file: ", sig_file, " at xbj=", xbj_bin, mass_scheme)
             # export_discrete(None, xbj_bin, data_sigmar, Path(sig_file).stem, sigma02, include_dipole=False, use_charm=use_charm)
-            ret = export_discrete_uniform(None, xbj_bin, data_sigmar, Path(sig_file).stem, sigma02, include_dipole=False, use_charm=use_charm, use_unity_sigma0=use_unity_sigma0)
-        print("Export done with:", use_charm, use_real_data)
+            ret = export_discrete_uniform(None, mass_scheme, xbj_bin, data_sigmar, Path(sig_file).stem, include_dipole=False, use_unity_sigma0=use_unity_sigma0)
+        print("Export done with:", mass_scheme, use_real_data)
         # exit()
     else:
         # Simulated data
+        print("Simulated data with quark mass schemes not implemented! Exit!")
+        exit()
         for dip_file in dipole_files:
             xbj_bin = float(Path(dip_file).stem.split("xbj")[1])
             data_sigmar_binned = data_sigmar[data_sigmar["xbj"]==xbj_bin]
@@ -378,7 +365,7 @@ def run_export(use_charm, use_real_data, fitname_i=None):
                 continue
             print("Discretizing forward problem for dipole file: ", dip_file, " at xbj=", xbj_bin, ", Datapoints N=", data_sigmar_binned.size)
             # export_discrete(data_path+dip_file, xbj_bin, data_sigmar_binned, Path(sigmar_files[0]).stem, sigma02, use_charm=use_charm)
-            ret = export_discrete_uniform(data_path+dip_file, xbj_bin, data_sigmar_binned, Path(sigmar_files[0]).stem, sigma02, use_charm=use_charm, use_unity_sigma0=use_unity_sigma0)
+            ret = export_discrete_uniform(data_path+dip_file, xbj_bin, data_sigmar_binned, Path(sigmar_files[0]).stem, use_unity_sigma0=use_unity_sigma0)
         # exit()
 
     return ret
@@ -390,18 +377,17 @@ if __name__ == '__main__':
     i=0
 
     run_settings=[
-        # (False, False, 3),
-        (True, False, 3),
-        (False, False, 4),
-        (True, False, 4),
-        (False, True, 3),
-        (True, True, 3),
-        (False, True, 4),
-        (True, True, 4),
+        "standard",
+        "pole",
+        "mqMpole",
+        "mqmq"
     ]
 
+    fitname_i = None
+    use_real_data = True
+
     for setting in run_settings:
-        use_charm, use_real_data, fitname_i = setting
+        qmasses = setting 
 
         try:
             r0 = run_export(use_charm,use_real_data,fitname_i)
