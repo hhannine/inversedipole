93f2d5b329f1b8687aec7464e0c2b343cb5ef2c8
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2025-08-21 23:07:51 +0300

    Add e_f^2 to quark mass scheme data

--- quark_mass_schemes.py
+++ quark_mass_schemes.py
@@ -1,42 +1,44 @@
 # Predefined mass schemes to use in the discretization of the forward operator.
 # Pole mass values from Ref. https://arxiv.org/pdf/hep-ph/9712201
 
+# masses and fractional charge squared is recorded under the key for each quark as a tuple.
+
 mass_scheme_standard = {
-    "u": 0.14,
-    "d": 0.14,
-    "s": 0.14,
-    "c": 1.4,
-    "b": 4.9,
+    "u": (0.14, 4/9),
+    "d": (0.14, 1/9),
+    "s": (0.14, 1/9),
+    "c": (1.4, 4/9),
+    "b": (4.9, 1/9),
 }
 
 mass_scheme_pole = {
-    "u": 0.501,
-    "d": 0.517,
-    "s": 0.687,
-    "c": 1.59,
-    "b": 4.89,
+    "u": (0.501, 4/9),
+    "d": (0.517, 1/9),
+    "s": (0.687, 1/9),
+    "c": (1.59, 4/9),
+    "b": (4.89, 1/9),
 }
 
 mass_scheme_mq_Mpole = {
-    "u": 0.0307,
-    "d": 0.0445,
-    "s": 0.283,
-    "c": 1.213,
-    "b": 4.248,
+    "u": (0.0307, 4/9),
+    "d": (0.0445, 1/9),
+    "s": (0.283, 1/9),
+    "c": (1.213, 4/9),
+    "b": (4.248, 1/9),
 }
 
 mass_scheme_mq_mq = {
-    "u": 0.436,
-    "d": 0.448,
-    "s": 0.553,
-    "c": 1.302,
-    "b": 4.339,
+    "u": (0.436, 4/9),
+    "d": (0.448, 1/9),
+    "s": (0.553, 1/9),
+    "c": (1.302, 4/9),
+    "b": (4.339, 1/9),
 }
 
 mass_scheme_mcharm = {
-    "u": 0.00418,
-    "d": 0.00840,
-    "s": 0.1672,
-    "c": 1.302,
-    "b": 6.12,
+    "u": (0.00418, 4/9),
+    "d": (0.00840, 1/9),
+    "s": (0.1672, 1/9),
+    "c": (1.302, 4/9),
+    "b": (6.12, 1/9),
 }
