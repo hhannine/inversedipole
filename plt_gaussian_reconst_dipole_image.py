34f5259fc25555bdd70b157ba61167645aa0aa41
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2025-09-30 11:06:13 +0300

    Update plt_gaussian_reconst_dipole_image.py
    Update dipole 2D tomography plotting for new reconstruction features

--- plt_gaussian_reconst_dipole_image.py
+++ plt_gaussian_reconst_dipole_image.py
@@ -8,8 +8,7 @@ import numpy as np
 import scipy.integrate as integrate
 import scipy.special as special
 from scipy.interpolate import CubicSpline, InterpolatedUnivariateSpline
-from scipy.io import loadmat
-import hankel
+from scipy.io import loadmat, matlab
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -69,44 +68,42 @@ def main(plotvar="xbj"):
 
     ####################
     # Data filename settings
-    # data_path = "./reconstructions/"
-    data_path = "./reconstructions_IUSdip/"
+    data_path = "./reconstructions_hera_uq/"
     str_data = "sim_"
-    str_fit = fitname
-    str_flavor = "lightonly_"
-    str_flavor_c = "lightpluscharm_"
-    name_base = 'recon_out_'
+    s_str = "s318.1_"
+    str_fit = "dis_inclusive_" + s_str
+    str_flavor = "standard_"
+    # str_flavor_c = "lightpluscharm_"
+    r_steps = "384"
+    name_base = 'hera_recon_uq_'+r_steps+"_"
     if use_real_data:
-        str_data = "hera_"
-        str_fit = "data_only_"
+        str_data = "rec_hera_data_"
     if use_noise:
         name_base = 'recon_with_noise_out_'
-    
-    # lambda_type = "broad_"
-    # lambda_type = "semiconstrained_"
-    # lambda_type = "semicon2_"
-    lambda_type = "fixed_"
-    composite_fname = name_base+str_data+str_fit+str_flavor+lambda_type
-    composite_fname_c = name_base+str_data+str_fit+str_flavor_c+lambda_type
-    print(composite_fname, composite_fname_c)
+    q_cut_str = "no_Q_cut"
+
+    lambda_type = "lambdaSRN_"
+    composite_fname = name_base + str_data + str_fit + str_flavor + lambda_type + q_cut_str
+    # composite_fname_c = name_base+str_data+str_fit+str_flavor_c+lambda_type
+    print(composite_fname)
 
     # Reading data files
     recon_files = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and composite_fname in i]
-    recon_files_c = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and composite_fname_c in i]
+    # recon_files_c = [i for i in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, i)) and composite_fname_c in i]
     print(recon_files)
-    print(recon_files_c)
+    # print(recon_files_c)
     if not recon_files:
         print("No files found!")
         print(data_path, composite_fname)
         exit(None)
     xbj_bins = [float(Path(i).stem.split("xbj")[1]) for i in recon_files]
     recon_files = [x for _, x in sorted(zip(xbj_bins, recon_files))]
-    recon_files_c = [x for _, x in sorted(zip(xbj_bins, recon_files_c))]
+    # recon_files_c = [x for _, x in sorted(zip(xbj_bins, recon_files_c))]
     xbj_bins = sorted(xbj_bins)
     print(xbj_bins)
 
     data_list = [loadmat(data_path + fle) for fle in recon_files]
-    data_list_c = [loadmat(data_path + fle) for fle in recon_files_c]
+    # data_list_c = [loadmat(data_path + fle) for fle in recon_files_c]
     
     #######################
     # Reading data
@@ -114,60 +111,38 @@ def main(plotvar="xbj"):
     q_averages = [np.median(np.sqrt(qvals)) for qvals in Q2vals_grid] # Best?
 
     R = data_list[0]["r_grid"][0]
+    # print(R)
     XBJ = np.array(xbj_bins)
     x_fit_out = XBJ[XBJ>1e-2]
     rr, xx = np.meshgrid(R,XBJ)
     rrfit, xxfitout = np.meshgrid(R,x_fit_out)
 
-    real_sigma = data_list[0]["real_sigma"][0]
-    best_lambdas = [dat["best_lambda"][0] for dat in data_list]
-    lambda_list_list = [dat["lambda"][0].tolist() for dat in data_list]
-    mI_list = [lambda_list.index(best_lambda) for lambda_list, best_lambda in zip(lambda_list_list, best_lambdas)]
-    best_lambdas_c = [dat["best_lambda"][0] for dat in data_list_c]
-    lambda_list_list_c = [dat["lambda"][0].tolist() for dat in data_list_c]
-    mI_list_c = [lambda_list_c.index(best_lambda_c) for lambda_list_c, best_lambda_c in zip(lambda_list_list_c, best_lambdas_c)]
-    if lambda_type in ["semiconstrained_", "fixed_"]:
-        uncert_i = [range(0, 5) for mI in mI_list]
-    else:
-        ucrt_step = 2
-        uncert_i = [range(mI-2*ucrt_step, mI+1+2*ucrt_step, ucrt_step) for mI in mI_list]
-
-    N_max_data = [dat["N_maxima"][0] for dat in data_list]
-    N_bpluseps_max_data = [dat["N_bpluseps_maxima"][0] for dat in data_list]
-    N_bminuseps_max_data = [dat["N_bminuseps_maxima"][0] for dat in data_list]
-    Nc_max_data = [dat["N_maxima"][0] for dat in data_list_c]
-    Nc_bpluseps_max_data = [dat["N_bpluseps_maxima"][0] for dat in data_list_c]
-    Nc_bminuseps_max_data = [dat["N_bminuseps_maxima"][0] for dat in data_list_c]
-
+    # real_sigma = data_list[0]["real_sigma"][0] # not needed, N_fit has it
     
-    # PROPER PLOTS
-    # 1. reconstruction from simulated data (light only)
-    #       - dipole vs reconstruction
-    # 2. simu sigmar data vs data from reconst
-    # 3. dipole rec with charm (simulated)
-    # 4. simu sigmar rec with charm
-    # 5. dipole reconstruction from HERA, light only
-    # 6. HERA sigmar vs sigmar(fit) vs sigmar(reconst), light only
-    # 7. dipole reconstruction from HERA, light plus charm
-    # 8. HERA sigmar vs sigmar(fit) vs sigmar(reconst), light plus charm
-    # 9. Sigma0(xbj) plot from real data reconstruction (or in W^2?)
-    #       - lightonly rec vs lightpluscharm rec vs horizontal line from fits (how about average of rec values?)
-    # 10. 2D dipole comparison in (r, xbj) or (r, W^2)
-    #       - fit vs real rec lightonly vs real rec lightpluscharm (3 plots/figures/images)
+    N_max_data = np.array([dat["N_max_data_strict"][0] for dat in data_list]) # this is used for the colorization scaling
+    N_max_data = N_max_data[:,0]
+    sig_max = max(N_max_data)
+    print(sig_max)
+    # print(N_max_data[:,0])
+    # max_N_max = max(N_max_data)
+
 
 
     ####################
     ### PLOT TYPE DIPOLE IMAGE
     ####################
 
-    fig, axs = plt.subplots(nrows=1, ncols=3)
-    fig.set_size_inches(20, 7)
+    fig, axs = plt.subplots(nrows=1, ncols=2)
+    # fig, axs = plt.subplots(nrows=1, ncols=3)
+    fig.set_size_inches(12, 5)
+    # fig.set_size_inches(20, 7)
     plt.tight_layout()
-    plt.subplots_adjust(top = 0.95, bottom = 0.12, right = 1.09, left = 0.06, hspace = 0, wspace = 0)
+    plt.subplots_adjust(top = 0.945, bottom = 0.13, right = 1.05, left = 0.07, hspace = 0, wspace = 0.03)
 
-    titles = [r"$\mathrm{Fit ~ dipole}$",
-              r"$\mathrm{Reconstructed ~ dipole, ~ light ~ only}$",
-              r"$\mathrm{Reconstructed ~ dipole, ~ light + charm}$",
+    titles = [r"$\mathrm{Fit ~ parametrization ~ dipole}$",
+            #   r"$\mathrm{HERA ~ reconstruction, ~ standard ~ scheme ~ u,d,s,c,b}$",
+              r"$\mathrm{Reconstruction ~ from ~ HERA ~ DIS ~ data}$",
+            #   r"$\mathrm{Reconstructed ~ dipole, ~ light + charm}$",
               "",
               ]
     if plotvar == "r":
@@ -178,17 +153,18 @@ def main(plotvar="xbj"):
             ax1.set_xscale('log')
             ax1.set_yscale('log')
             ax1.grid(False)
-            ax1.tick_params(which='major',width=1,length=6,labelsize=20)
-            ax1.tick_params(which='minor',width=0.7,length=4,labelsize=20)
+            ax1.tick_params(which='major',width=1,length=6,labelsize=14)
+            ax1.tick_params(which='minor',width=0.7,length=4,labelsize=14)
             ax1.tick_params(axis='both', pad=7)
             ax1.tick_params(axis='both', which='both', direction="in")
-            ax1.set_xlim([1e-1, max(R)])
-            ax1.set_ylim([min(XBJ), max(XBJ)])
+            # ax1.set_xlim([1e-2, max(R)])
+            ax1.set_xlim([min(R), max(R)])
+            # ax1.set_ylim([min(XBJ), max(XBJ)])
             # ax1.set_title(titles[i], fontsize=20, pad=10)
-            ax1.set_title(titles[i], fontsize=10, pad=1)
-            ax1.set_xlabel(r'$r ~ \left( \mathrm{GeV}^{-1} \right)$', fontsize=22)
+            ax1.set_title(titles[i], fontsize=16, pad=4)
+            ax1.set_xlabel(r'$r ~ \left( \mathrm{GeV}^{-1} \right)$', fontsize=18)
             if i==0:
-                ax1.set_ylabel(r'$x_{\mathrm{Bj.}}$', fontsize=22)
+                ax1.set_ylabel(r'$x_{\mathrm{Bj.}}$', fontsize=26)
             elif i!=0:
                 ax1.set_yticks([])
 
@@ -204,87 +180,124 @@ def main(plotvar="xbj"):
     elif plotvar=="Q":
         xvar = np.array(q_averages)
 
-    Nlight_max = np.array([max_vals[mI] for mI, max_vals in zip(mI_list, N_max_data)])
-    Nlight_bplus_max = np.array([max_vals[mI] for mI, max_vals in zip(mI_list, N_bpluseps_max_data)])
-    Nlight_bminus_max = np.array([max_vals[mI] for mI, max_vals in zip(mI_list, N_bminuseps_max_data)])
-    Nlight_err_upper = Nlight_bplus_max - Nlight_max
-    Nlight_err_lower = Nlight_max - Nlight_bminus_max
-    Nlight_errs = np.array([gev_to_mb*Nlight_err_lower, gev_to_mb*Nlight_err_upper])
-
-    Ncharm_max = np.array([max_vals[mI] for mI, max_vals in zip(mI_list_c, Nc_max_data)])
-    Ncharm_bplus_max = np.array([max_vals[mI] for mI, max_vals in zip(mI_list_c, Nc_bpluseps_max_data)])
-    Ncharm_bminus_max = np.array([max_vals[mI] for mI, max_vals in zip(mI_list_c, Nc_bminuseps_max_data)])
-    Ncharm_err_upper = Ncharm_bplus_max - Ncharm_max
-    Ncharm_err_lower = Ncharm_max - Ncharm_bminus_max
-    Ncharm_errs = np.array([gev_to_mb*Ncharm_err_lower, gev_to_mb*Ncharm_err_upper])
-
-    x_srted = sorted(xvar)
-    x_range = np.logspace(-2, -0.5, 3)
-    q_range = np.linspace(9.6, 10.6, 1)
-    q_range2 = np.linspace(10.2, 10.6, 1, endpoint=False)
-    target_radii = np.sqrt(Ncharm_max/(10*math.pi)) # divide millibarn/10 to get square femtometers
-
     dip_data_rec = np.array([dat["N_reconst"] for dat in data_list]) # data_list is indexed the same as xbj_bins, each N_rec is indexed in r_grid
-    dip_data_rec_c = np.array([dat["N_reconst"] for dat in data_list_c]) # data_list is indexed the same as xbj_bins, each N_rec is indexed in r_grid
-    dip_data_fit = np.array([dat["N_fit"] for dat in data_list]) # data_list is indexed the same as xbj_bins, each N_rec is indexed in r_grid
+    # for dat in data_list:
+    # dip_data_rec_c = np.array([dat["N_reconst"] for dat in data_list_c]) # data_list is indexed the same as xbj_bins, each N_rec is indexed in r_grid
+    # dip_data_fit = np.array([dat["N_fit"] for dat in data_list])
+    dip_data_fit = []
+    for xbj, dat in zip(xbj_bins, data_list):
+        if xbj < 0.01:
+            dip_data_fit.append(dat["N_fit"][:,0])
+        else:
+            a = np.array(dat["r_steps"][0][0] * [0])
+            dip_data_fit.append(a)
+    dip_data_fit = np.array(dip_data_fit)
+
+    real_sigma = max(dip_data_fit[0])
+    print(real_sigma)
     reshape_fit = dip_data_fit.reshape((len(XBJ), len(R)))
     reshape_dip = dip_data_rec.reshape((len(XBJ), len(R)))
-    reshape_dip_c = dip_data_rec_c.reshape((len(XBJ), len(R)))
+    # reshape_dip_c = dip_data_rec_c.reshape((len(XBJ), len(R)))
 
-    # mapname = 'plasma'
-    mapname = 'magma'
+    # mapname = 'viridis'
+    # mapname = 'magma'
+    # mapname = 'copper'
+    # mapname = 'gist_heat'
+    # mapname = 'bone'
+    mapname = 'coolwarm'
+    # mapname = 'RdBu'
     cmap = plt.colormaps[mapname]
     
-    norm = plt.Normalize(np.min(Ncharm_max), np.max(Ncharm_max))
+    norm = plt.Normalize(np.min(N_max_data), np.max(N_max_data))
     # norm = mpl.colors.LogNorm(np.min(Ncharm_max), np.max(Ncharm_max))
     # norm = mpl.colors.LogNorm(np.min(Ncharm_max), np.max(Ncharm_max))
-    smap = plt.cm.ScalarMappable(cmap='plasma', norm=norm)
+    smap = plt.cm.ScalarMappable(cmap=mapname, norm=norm)
+    log_scale_col = True
+    log_scale=400
+    sig_max *= gev_to_mb
+    lognorm = mpl.colors.LogNorm(sig_max/log_scale, sig_max)
 
-    print(np.min(Ncharm_max)/10, np.max(Ncharm_max))
+    # print(np.min(Ncharm_max)/10, np.max(Ncharm_max))
+    plot_rad_line = False
 
-    target_radii = np.sqrt(Nlight_max/(10*math.pi)) # divide millibarn/10 to get square femtometers
-    target_radii_c = np.sqrt(Ncharm_max/(10*math.pi)) # divide millibarn/10 to get square femtometers
+    target_radii = np.sqrt(N_max_data/(10*math.pi)) # divide millibarn/10 to get square femtometers
+    # target_radii_c = np.sqrt(Ncharm_max/(10*math.pi)) # divide millibarn/10 to get square femtometers
     fm_to_gev = 5.068
     radii_in_gev = target_radii*fm_to_gev
-    radii_in_gev_c = target_radii_c*fm_to_gev
+    # radii_in_gev_c = target_radii_c*fm_to_gev
     sigma0_to_rad_fm = np.sqrt(real_sigma*gev_to_mb/(10*math.pi))*fm_to_gev
 
     max_adj_mult = 1
-    clim=[0,max(Ncharm_max)]
-    ax = axs[0] 
-    cfit = ax.pcolormesh(rr, xx, real_sigma*reshape_fit, vmin=0, vmax=max(Ncharm_max)*max_adj_mult, cmap = cmap) 
-    cfitbox = ax.pcolormesh(rrfit, xxfitout, np.zeros((len(x_fit_out), len(R))), vmin=0, vmax=max(Ncharm_max)*max_adj_mult, cmap = cmap) 
-    cfit.norm.autoscale(clim)
-    ax.plot([sigma0_to_rad_fm]*len(xbj_bins), xbj_bins, marker="s", c="white")
-    # cfit = ax.pcolormesh(rr, xx, real_sigma*reshape_fit, cmap = cmap, norm = mpl.colors.LogNorm(np.min(Ncharm_max)/10, np.max(Ncharm_max))) 
-    ax = axs[1] 
-    c = ax.pcolormesh(rr, xx, reshape_dip, vmin=0, vmax=max(Ncharm_max)*max_adj_mult, cmap = cmap) 
-    c.norm.autoscale(clim)
-    ax.plot(radii_in_gev, xbj_bins, marker="s", c="white")
-    # c = ax.pcolormesh(rr, xx, reshape_dip+0.6, cmap = cmap, norm = mpl.colors.LogNorm(np.min(Ncharm_max)/10, np.max(Ncharm_max)))  
-    ax = axs[2] 
-    cc = ax.pcolormesh(rr, xx, reshape_dip_c, vmin=0, vmax=max(Ncharm_max)*max_adj_mult, cmap = cmap) 
-    cc.norm.autoscale(clim)
-    ax.plot(radii_in_gev_c, xbj_bins, marker="s", c="white")
+    # clim=[0,sig_max]
+    ax = axs[0]
+    ax.grid(False, which="both")
+    # cfit = ax.pcolormesh(rr, xx, real_sigma*reshape_fit, vmin=0, vmax=max(N_max_data)*max_adj_mult, cmap = cmap) # new export has real_sigma, no need to multiply it anymore
+    # cfit = ax.pcolormesh(rr, xx, gev_to_mb*reshape_fit, vmin=0, vmax=max(N_max_data)*max_adj_mult, cmap = cmap)
+    if log_scale_col:
+        cfit = ax.pcolormesh(rr, xx, gev_to_mb*reshape_fit, cmap = cmap, norm = lognorm,linewidth=0,rasterized=True)
+        cfit.set_edgecolor('face')
+    cfitbox = ax.pcolormesh(rrfit, xxfitout, np.zeros((len(x_fit_out), len(R))), vmin=0, vmax=max(N_max_data)*max_adj_mult, cmap = "inferno", linewidth=0,rasterized=True) 
+    cfitbox.set_edgecolor('face')
+    # cfit.norm.autoscale(clim)
+    if plot_rad_line:
+        ax.plot([sigma0_to_rad_fm]*len(xbj_bins), xbj_bins, marker="o", markersize=3.5, c="white")
+    ax = axs[1]
+    ax.grid(False, which="both")
+    if log_scale_col:
+        missing_data_bg = ax.pcolormesh(rr, xx, np.zeros((len(xbj_bins), len(R))), vmin=0, vmax=max(N_max_data)*max_adj_mult, cmap = "inferno", linewidth=0,rasterized=True)
+        missing_data_bg.set_edgecolor('face')
+    # c = ax.pcolormesh(rr, xx, reshape_dip, vmin=0, vmax=max(N_max_data)*max_adj_mult, cmap = cmap) 
+    c = ax.pcolormesh(rr, xx, gev_to_mb*reshape_dip, cmap = cmap, norm = lognorm,linewidth=0,rasterized=True)
+    c.set_edgecolor('face')
+    # c.norm.autoscale(clim)
+    if plot_rad_line:
+        ax.plot(radii_in_gev, xbj_bins, marker="o", markersize=3.5, c="white")
+    # ax = axs[2] 
+    # cc = ax.pcolormesh(rr, xx, reshape_dip_c, vmin=0, vmax=max(Ncharm_max)*max_adj_mult, cmap = cmap) 
+    # cc.norm.autoscale(clim)
+    # ax.plot(radii_in_gev_c, xbj_bins, marker="s", c="white")
     # cc = ax.pcolormesh(rr, xx, reshape_dip_c+0.9, cmap = cmap, norm = mpl.colors.LogNorm(np.min(Ncharm_max)/10, np.max(Ncharm_max))) 
     
-    cbar=fig.colorbar(cc, ax=axs.ravel().tolist(), shrink=1, pad=0.02)
+    cbar=fig.colorbar(c, ax=axs.ravel().tolist(), shrink=1, pad=0.01)
     cbar.set_label(r'$\frac{\sigma_0}{2} N(r) ~ \left(\mathrm{mb}\right)$', fontsize=22)
     cbar.ax.tick_params(labelsize=15) 
-    # cc.clim(0, 50)
+
 
     data_rec_point_c = Line2D([0,1],[0,1],linestyle='-', marker="", color="white")
+    noise_fail = Patch(color="Black")  #Patch(facecolor=colors[col_i], alpha=0.3)
 
-    manual_handles = [
-                        data_rec_point_c,
-                        ]
-    manual_labels = [
-        # r'${\frac{r}{r_g} = 1}$',
-        r'${r = r_g}$',
-    ]
-    leg = axs[0].legend(manual_handles, manual_labels, frameon=False, fontsize=24, ncol=1, loc="upper left") 
+    if plot_rad_line:
+        manual_handles = [
+                            data_rec_point_c,
+                            noise_fail
+                            ]
+        manual_labels = [
+            r'${r = r_g}$',
+            r'$\mathrm{Rec. ~ noise}$',
+        ]
+    else:
+        manual_handles = [
+                            noise_fail
+                            ]
+        manual_labels = [
+            r'$\mathrm{Rec. ~ noise ~/~ exclusion ~ from ~ fit}$',
+        ]
+    leg = axs[0].legend(manual_handles, manual_labels, frameon=True, fontsize=20, ncol=1, loc="upper left") 
     for txt in leg.get_texts():
-        txt.set_color("white")
+        # txt.set_color("white")
+        txt.set_color("black")
+    
+    prelim = True
+    if prelim:
+        x_crd = 0.04
+        y_crd = 0.97
+        prelim_lbl = r"$\mathrm{Preliminary}$"
+        axs.flatten()[1].text(x_crd, y_crd, prelim_lbl,
+                horizontalalignment="left",
+                verticalalignment='top',
+                fontsize=24,
+                color="red",
+                transform=axs.flatten()[i].transAxes)
 
     if plotvar=="r":
         n_plot = "plot10-r-dipoleimage-"
@@ -292,16 +305,16 @@ def main(plotvar="xbj"):
         print("Plot number?")
         exit()
 
-    # write2file = False
+    write2file = False
     write2file = True
     if write2file:
         mpl.use('agg') # if writing to PDF
         plt.draw()
         outfilename = n_plot + name_base+str_data+str_fit+lambda_type + "{}".format(PLOT_TYPE) + '.pdf'
         # outfilename = n_plot + composite_fname + "{}".format(PLOT_TYPE) + '.png'
-        plotpath = G_PATH+"/inversedipole/plots/"
+        plotpath = G_PATH+"/inversedipole/plots_hera_uq/"
         print(os.path.join(plotpath, outfilename))
-        plt.savefig(os.path.join(plotpath, outfilename))
+        plt.savefig(os.path.join(plotpath, outfilename),dpi=300)
     else:
         plt.margins(0,0)
         plt.show()
