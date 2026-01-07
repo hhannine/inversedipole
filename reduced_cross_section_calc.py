06da374ee61eced98c95e42d2deb25cee6e7e720
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2026-01-07 15:41:24 +0200

    Create reduced_cross_section_calc.py

---
+++ reduced_cross_section_calc.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+inversedipole implementation written in collaboration by H. Hänninen, H. Schlüter
+Copyright 2026
+
+Computes reduced DIS cross sections in the dipole picture from arbitrary evolved dipole amplitude.
+"""
