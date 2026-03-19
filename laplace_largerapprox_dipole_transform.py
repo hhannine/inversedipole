fcdeba1532536d3ea07fa64cd5664f55d77acf42
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2026-03-19 23:01:34 +0200

    Create laplace_largerapprox_dipole_transform.py

---
+++ laplace_largerapprox_dipole_transform.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Laplace transform based approximation of the dipole transform implementation written by H. Hänninen
+in collaboration with J. Ilmavirta, A. Kykkänen
+Copyright 2026
+"""
+
+
+def forward_dip_laplace_transform():
+    "Assumes the limit r>>1, m=0, z=½"
+    todo = True
+    return
+
+
+def inverse_dip_laplace_transform():
+    "Assumes the limit r>>1, m=0, z=½"
+    todo = True
+    return
+
+
+def forward_dip_laplace_transform_zint():
+    """Assumes the limit r>>1, m=0. z is integrated over [0,1].
+    
+    No explicit inversion method yet."""
+    todo = True
+    return
+
